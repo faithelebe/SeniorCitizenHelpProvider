f8a65764057fa544a74cb021bdc99984acc86bb4
faithelebe <elebefaith@yahoo.com>
2018-11-17 09:04:07 +0800

    Update updateRatingRequest.php
    update

--- Updated webSite/updateRatingRequest.php
+++ Updated webSite/updateRatingRequest.php
@@ -8,3 +8,4 @@ $ccomment =$_POST['ratingComment'];
 $sql = "UPDATE sentrequest SET Note = '{$note}' , status = '{$updatedStatus}', rating = '{$rating}'
 , ratingComment = '{$ccomment}' WHERE id = '{$id}'";
 $conn->query($sql);
+header('Location: http://localhost/bit216/newManageRequest.php');
