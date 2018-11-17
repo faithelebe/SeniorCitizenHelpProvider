93e143512c48bce800c2f491ae07eb90542fc468
faithelebe <elebefaith@yahoo.com>
2018-11-17 10:49:21 +0800

    Update manageRequest.php
    update

--- Updated webSite/manageRequest.php
+++ Updated webSite/manageRequest.php
@@ -54,3 +54,7 @@ $usernameSenior = $_SESSION['user_name'];
            background: #4e54c8;  /* fallback for old browsers */
            background: -webkit-linear-gradient(to right, #8f94fb, #4e54c8);  /* Chrome 10-25, Safari 5.1-6 */
            background: linear-gradient(to right, #8f94fb, #4e54c8); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            }
+         /* Full-width inputs */
+         input[type=text], input[type=password] {
+         width: 50%;
