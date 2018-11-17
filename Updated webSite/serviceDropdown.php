11ed24cae48fe5bd7885a7640ad58cbc1e1f4fe4
faithelebe <elebefaith@yahoo.com>
2018-11-17 09:11:44 +0800

    Update serviceDropdown.php
    update

--- Updated webSite/serviceDropdown.php
+++ Updated webSite/serviceDropdown.php
@@ -3,3 +3,4 @@ require "./database.php";  //connect to the database
 $q = "SELECT * FROM servicetable;";//retrieve all records from  servicetable
 $result = $conn->query($q);//execute and store the result of sql query in variable $result
 if($result){  //$result should be an object if successful
+	$rows  = $result->fetch_all(MYSQLI_ASSOC);
