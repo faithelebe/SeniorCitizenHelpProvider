0ed058fcee2f45bd50cf08f05099ce67642ae792
faithelebe <elebefaith@yahoo.com>
2018-11-17 09:12:12 +0800

    Update serviceDropdown.php
    update

--- Updated webSite/serviceDropdown.php
+++ Updated webSite/serviceDropdown.php
@@ -4,3 +4,4 @@ $q = "SELECT * FROM servicetable;";//retrieve all records from  servicetable
 $result = $conn->query($q);//execute and store the result of sql query in variable $result
 if($result){  //$result should be an object if successful
 	$rows  = $result->fetch_all(MYSQLI_ASSOC);
+	print('<select id = "codes" name="serviceCode">');
