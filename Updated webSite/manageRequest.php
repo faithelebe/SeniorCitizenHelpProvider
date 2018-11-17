724d4f7bee15d37d1d0f926382272ab8c9e803cc
faithelebe <elebefaith@yahoo.com>
2018-11-17 10:38:55 +0800

    Update manageRequest.php
    update

--- Updated webSite/manageRequest.php
+++ Updated webSite/manageRequest.php
@@ -25,3 +25,5 @@ $usernameSenior = $_SESSION['user_name'];
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <!--Include bootstrap library as needed-->
       <script src="js/bootstrap.min.js"></script>
+      <style>
+         body, html {
