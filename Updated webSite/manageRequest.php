3206cde4a60358aaa99126b03c1a6e12a18d3a9d
faithelebe <elebefaith@yahoo.com>
2018-11-17 10:56:45 +0800

    Update manageRequest.php
    update

--- Updated webSite/manageRequest.php
+++ Updated webSite/manageRequest.php
@@ -121,3 +121,9 @@ $usernameSenior = $_SESSION['user_name'];
          display: block;
          float: none;
          }
+         .cancelbtn {
+         width: 50%;
+         }
+         }
+      </style>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
